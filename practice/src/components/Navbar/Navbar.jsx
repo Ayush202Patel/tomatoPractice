ca3b6953d1f43a103afa0b8c0d36b0414e6d6ad8
Ayush Patel <ayushpatel2005.2@gmail.com>
2025-10-29 23:18:56 +0530

    Completed without guidance

--- practice/src/components/Navbar/Navbar.jsx
+++ practice/src/components/Navbar/Navbar.jsx
@@ -4,23 +4,19 @@ import { assets } from "../../assets/assets";
 const Navbar = () => {
   return (
     <div className="navbar">
-      <img src={assets.logo} alt="" className="logo" />
+      <img src={assets.logo} className="logo" />
       <ul className="navbar-menu">
         <li>home</li>
         <li>menu</li>
-        <li>mobile app</li>
+        <li>mobile-app</li>
         <li>contact us</li>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
-        <div className="navbar-search-icon">
-          <img src={assets.basket_icon} alt="" />
-          <div className="dot"></div>
-        </div>
-        <button>Sign in</button>
+        <img src={assets.basket_icon} alt="" />
+        <button>sign in</button>
       </div>
     </div>
   );
 };
-
 export default Navbar;

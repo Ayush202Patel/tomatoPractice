08ce8225bca8a84608a465c4ed6e71508caef174
Ayush Patel <ayushpatel2005.2@gmail.com>
2025-11-05 22:45:45 +0530

    ERROR

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Header from "./components/Header/Header";
 import Search from "./components/Search/Search";
+import FoodItem from "./components/FoodItem/FoodItem";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Search />
       <Navbar />
       <Header />
+      <FoodItem />
     </div>
   );
 };

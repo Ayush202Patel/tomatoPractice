820b5ea2428c4046ea2f0b0b06082a5ea2ae3d6b
Ayush Patel <ayushpatel2005.2@gmail.com>
2025-11-06 23:45:33 +0530

    Finally running

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -2,15 +2,14 @@ import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Header from "./components/Header/Header";
 import Search from "./components/Search/Search";
-import FoodItem from "./components/FoodItem/FoodItem";
-
+import FoodDisplay from "./components/FoodDisplay/FoodDisplay";
 const App = () => {
   return (
     <div className="App">
       <Search />
       <Navbar />
       <Header />
-      <FoodItem />
+      <FoodDisplay />
     </div>
   );
 };

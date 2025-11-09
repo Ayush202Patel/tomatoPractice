a3db9f4a715dff91e2f75e318a451dcc38315c73
Ayush Patel <ayushpatel2005.2@gmail.com>
2025-11-09 09:30:23 +0530

    NAvbar

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -1,15 +1,9 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
-import Header from "./components/Header/Header";
-import Search from "./components/Search/Search";
-import FoodDisplay from "./components/FoodDisplay/FoodDisplay";
 const App = () => {
   return (
     <div className="App">
-      <Search />
       <Navbar />
-      <Header />
-      <FoodDisplay />
     </div>
   );
 };
---
+++ practice/src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Header.css";
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>Order Your favourtie food here</h1>
+      <p>
+        Choose from a diverse menu featuring a delectable array of dishes
+        crafted with the finest ingrideints and culinary expertise.Our mission
+        is to satisfy your carvings and elevate your dining experience,one
+        delecious meal at a time
+      </p>
+      <button>View Menu</button>
+    </div>
+  );
+};
+
+export default Header;
